367c341fb712707e2a57f823712b379d84c1dd0c
josefigueiredo <deb.gnulinux@gmail.com>
2023-05-13 18:57:56 -0300

    removido arquivos necessarios do git ignore

---
+++ Micro Apostila/images.pl
@@ -0,0 +1,215 @@
+# LaTeX2HTML 2021 (Released January 1, 2021)
+# Associate images original text with physical files.
+
+
+$key = q//;
+$cached_env_img{$key} = q|<IMG
+ STYLE=""
+ SRC="|."$dir".q|img21.svg"
+ ALT="\begin{pseudocode}
+Inicio
+var repetir, valor, media, i =0, sum = 0 ;
+repetir = l...
+...um + valor;
+fim-enquanto
+media = sum/repetir
+mostrar(media)
+Fim
+\end{pseudocode}">;WIDTH="0" HEIGHT="0"|; 
+
+$key = q/-1;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ STYLE="height: 1.86ex; vertical-align: -0.30ex; " SRC="|."$dir".q|img25.svg"
+ ALT="$-1$">|; 
+
+$key = q/ITERAÇÃO;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ STYLE="height: 2.58ex; vertical-align: -0.56ex; " SRC="|."$dir".q|img1.svg"
+ ALT="$ITERAÇÃO$">|; 
+
+$key = q/Mostra(media);MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ STYLE="height: 2.53ex; vertical-align: -0.69ex; " SRC="|."$dir".q|img20.svg"
+ ALT="$Mostra(media)$">|; 
+
+$key = q/continuar;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ STYLE="height: 1.74ex; vertical-align: -0.11ex; " SRC="|."$dir".q|img24.svg"
+ ALT="$continuar$">|; 
+
+$key = q/do-until;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ STYLE="height: 2.01ex; vertical-align: -0.30ex; " SRC="|."$dir".q|img28.svg"
+ ALT="$do-until$">|; 
+
+$key = q/enquantoi<repetir;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ STYLE="height: 2.19ex; vertical-align: -0.56ex; " SRC="|."$dir".q|img16.svg"
+ ALT="$enquanto i&lt;repetir$">|; 
+
+$key = q/i;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ STYLE="height: 1.74ex; vertical-align: -0.11ex; " SRC="|."$dir".q|img7.svg"
+ ALT="$i$">|; 
+
+$key = q/i=i+1;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ STYLE="height: 1.93ex; vertical-align: -0.30ex; " SRC="|."$dir".q|img12.svg"
+ ALT="$i = i + 1$">|; 
+
+$key = q/media;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ STYLE="height: 1.82ex; vertical-align: -0.11ex; " SRC="|."$dir".q|img8.svg"
+ ALT="$media$">|; 
+
+$key = q/media=sumslashrepetir;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ STYLE="height: 2.53ex; vertical-align: -0.69ex; " SRC="|."$dir".q|img19.svg"
+ ALT="$media=sum/repetir$">|; 
+
+$key = q/num;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ STYLE="height: 1.21ex; vertical-align: -0.11ex; " SRC="|."$dir".q|img9.svg"
+ ALT="$num$">|; 
+
+$key = q/num=lerValor();MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ STYLE="height: 2.53ex; vertical-align: -0.69ex; " SRC="|."$dir".q|img10.svg"
+ ALT="$num = lerValor()$">|; 
+
+$key = q/num=lerVavlor();MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ STYLE="height: 2.53ex; vertical-align: -0.69ex; " SRC="|."$dir".q|img18.svg"
+ ALT="$num = lerVavlor()$">|; 
+
+$key = q/repetir;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ STYLE="height: 2.19ex; vertical-align: -0.56ex; " SRC="|."$dir".q|img17.svg"
+ ALT="$repetir$">|; 
+
+$key = q/sum;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ STYLE="height: 1.21ex; vertical-align: -0.11ex; " SRC="|."$dir".q|img6.svg"
+ ALT="$sum$">|; 
+
+$key = q/sum=sum+num;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ STYLE="height: 1.75ex; vertical-align: -0.30ex; " SRC="|."$dir".q|img11.svg"
+ ALT="$sum = sum + num$">|; 
+
+$key = q/while;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ STYLE="height: 1.82ex; vertical-align: -0.11ex; " SRC="|."$dir".q|img2.svg"
+ ALT="$while$">|; 
+
+$key = q/{code}cleardodisp("Continuar....")parar=input(-1parafinalizar:")until(parar==-1){code};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ STYLE=""
+ SRC="|."$dir".q|img29.svg"
+ ALT="\begin{code}
+clear
+do
+disp(''Continuar....'')
+parar = input(-1 para finalizar: '')
+until(parar == -1)
+\end{code}">;WIDTH="0" HEIGHT="0"|; 
+
+$key = q/{code}cleari=1,sum=0,num=0,media=0while(i<=5)num=input("digiteumnumero")sum=sum+numi++endwhilemedia=sumslash5printf("<comment_mark>7{code};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ STYLE=""
+ SRC="|."$dir".q|img15.svg"
+ ALT="\begin{code}
+clear
+i = 1, sum = 0, num = 0, media = 0
+while (i &lt;= 5)
+num = inpu...
+... sum = sum + num
+i++
+endwhile
+media = sum / 5
+printf('' %d '',media)
+\end{code}">;WIDTH="0" HEIGHT="0"|; 
+
+$key = q/{code}cleari=1while(i<=10)printf("<comment_mark>6i++endwhile{code};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ STYLE=""
+ SRC="|."$dir".q|img5.svg"
+ ALT="\begin{code}
+clear
+i = 1
+while (i &lt;= 10)
+printf('' %d '',i)
+i++
+endwhile
+\end{code}">;WIDTH="0" HEIGHT="0"|; 
+
+$key = q/{code}vari=1,sum=0,num,media;while(i<=25){num=parseInt(prompt());sum=sum+num;i=i+1;}media=sumslash25;print(media);{code};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ STYLE=""
+ SRC="|."$dir".q|img14.svg"
+ ALT="\begin{code}
+var i = 1,sum = 0,num,media;
+while( i &lt;= 25){
+num = parseInt(prompt());
+sum = sum + num;
+i = i + 1;
+}
+media = sum / 25;
+print(media);
+\end{code}">;WIDTH="0" HEIGHT="0"|; 
+
+$key = q/{code}vari=1;while(i<=10){print(i);i=i+1;}{code};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ STYLE=""
+ SRC="|."$dir".q|img4.svg"
+ ALT="\begin{code}
+var i = 1;
+while(i &lt;= 10){
+print(i);
+i = i + 1;
+}
+\end{code}">;WIDTH="0" HEIGHT="0"|; 
+
+$key = q/{code}varparar;do{print("continuar");parar=prompt(-1finalizarslashoutrovalorparacontinuar");}while(parar!=-1);{code};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ STYLE=""
+ SRC="|."$dir".q|img27.svg"
+ ALT="\begin{code}
+var parar;
+do{
+print(''continuar'');
+parar = prompt(-1 finalizar / outro valor para continuar'');
+}
+while(parar != -1);
+\end{code}">;WIDTH="0" HEIGHT="0"|; 
+
+$key = q/{pseudocode}Iniciovari=1enquantoi<=10,repita:mostraii=i+1fim-enquantoFim{pseudocode};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ STYLE=""
+ SRC="|."$dir".q|img3.svg"
+ ALT="\begin{pseudocode}
+Inicio
+var i = 1
+enquanto i &lt;= 10, repita:
+mostra i
+i = i + 1
+fim-enquanto
+Fim
+\end{pseudocode}">;WIDTH="0" HEIGHT="0"|; 
+
+$key = q/{pseudocode}Iniciovarparar;facamostrar("continuar");parar=lerValor();enquanto(parar!=-1)Fim{pseudocode};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ STYLE=""
+ SRC="|."$dir".q|img26.svg"
+ ALT="\begin{pseudocode}
+Inicio
+var parar;
+faca
+mostrar(''continuar'');
+parar = lerValor();
+enquanto (parar != -1)
+Fim
+\end{pseudocode}">;WIDTH="0" HEIGHT="0"|; 
+
+1;
+
---
+++ Micro Apostila/internals.pl
@@ -0,0 +1,90 @@
+# LaTeX2HTML 2021 (Released January 1, 2021)
+# Associate internals original text with physical files.
+
+
+$key = q/cap:fuxos/;
+$ref_files{$key} = "$dir".q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:fluxogramaproblema3/;
+$ref_files{$key} = "$dir".q|node17.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:laco-do-while/;
+$ref_files{$key} = "$dir".q|node21.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:lacosimples/;
+$ref_files{$key} = "$dir".q|node5.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:problema1/;
+$ref_files{$key} = "$dir".q|node7.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:problema2/;
+$ref_files{$key} = "$dir".q|node12.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:problemaDoWhile1/;
+$ref_files{$key} = "$dir".q|node23.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:problema___/;
+$ref_files{$key} = "$dir".q|node34.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:problema_do-while2_/;
+$ref_files{$key} = "$dir".q|node28.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/intro/;
+$ref_files{$key} = "$dir".q|node2.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/lacos/;
+$ref_files{$key} = "$dir".q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/problema_while1_/;
+$ref_files{$key} = "$dir".q|node6.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/problema_while2_/;
+$ref_files{$key} = "$dir".q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/problema_while3_/;
+$ref_files{$key} = "$dir".q|node16.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:dowhile/;
+$ref_files{$key} = "$dir".q|node21.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:problema_/;
+$ref_files{$key} = "$dir".q|node33.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:problema_do-while1_/;
+$ref_files{$key} = "$dir".q|node22.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:problema_do-while2_/;
+$ref_files{$key} = "$dir".q|node27.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:tipolaco/;
+$ref_files{$key} = "$dir".q|node32.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:while/;
+$ref_files{$key} = "$dir".q|node5.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:testemesa1/;
+$ref_files{$key} = "$dir".q|node12.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
